418275a9cdb1a36edc84967e620fbfed9837537b
Gildo <junioripg@gmail.com>
2019-10-22 13:30:07 -0400

    config do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,34 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
+  'env': {
+    'es6': true,
+    'node': true,
   },
-  extends: [
-    'airbnb-base',
+  'extends': [
+    'google'
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
   },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
   },
-  rules: {
+  'rules': {
+    'semi': ['error', 'always'],
+    'require-jsdoc': ['error', {
+      'require': {
+        'FunctionDeclaration': false,
+        'MethodDefinition': false,
+        'ClassDeclaration': false,
+        'ArrowFunctionExpression': false,
+        'FunctionExpression': false
+      }
+    }],
+    'linebreak-style': 0,
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'camelcase': 'off',
+    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
   },
 };
