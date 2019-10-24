413171d26b43ddeeeccd16bdbfbe64fffc745266
Gildo Jr <gildojr@MacBook-Air-de-Gildo.local>
2019-10-24 12:11:26 -0400

    sequelize data

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import {Router} from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middleware/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({message: 'hello world'});
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.post('/users', UserController.store);
 
 export default routes;
